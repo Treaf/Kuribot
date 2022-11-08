751052f9d42d31b00e9e3ed9d809db563513a9eb
Treaf <117280032+Treaf@users.noreply.github.com>
2022-11-08 14:12:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,13 +24,9 @@ const monthNames = ["january", "february", "march", "april", "may", "june",
 
 // Creation de commandes
 
-
-// Reponse "Heure du Duel"
-
-
 client.on("messageCreate", message => {
     if (message.author.bot) return;
-    
+   
     if (message.content === `!heure`){
         message.reply("C'est l'heure du Duel ! \nhttps://tenor.com/view/yu-gi-oh-time-to-duel-card-gif-12403495"
         );
@@ -38,157 +34,140 @@ client.on("messageCreate", message => {
 }
 );
 
-
-// Banlist TCG
+// News YUGIOH
+ 
 
 client.on("ready", message=>{
-    console.log('Bot pret.')
-    let timestop=true;
-    let repet=true;
-//    const idsalonnews="1036956441354981416"; // ID de TKT
-    const idsalonnews="964907351637368832"; // Mettre ID du salon des news
+    console.log("prêt");
+
+    // Timers
+    var timestopduellinks=false;
+    var timestopban=true;
+    var timestopmd=false;
+    var timestopcd=false;
+    var repetban=true;
+
+    // ID des salons des news
+    var idsalonnewstcg="964907351637368832"; 
+    var idsalonnewsdl="993787860048150569"; 
+    var idsalonnewsmd="993789778833195138"; 
+    var idsalonnewscd="993789979371249745"; 
+
+    var sitekonami="https://www.yugioh-card.com/fr/limited/";
+
     setInterval( function(){
-        let sitekonami="https://www.yugioh-card.com/fr/limited/";
-        needle.get(sitekonami, (error, response) => {
-            let date = new Date();
-            var dateajd = new Date(date.getFullYear()+'-'+monthNames[date.getMonth()]+'-'+date.getDate());
-            let text = response.body;
-            let avt= text.indexOf("Effectif du ");
-            var eff=text.substring(avt+12);
-            let ap= eff.indexOf('</h2>');
-            eff=eff.substring(0,ap);
-            var dateeff=new Date(eff.substring(6,10)+'-'+eff.substring(3,5)+'-'+eff.substring(0,2));
-        
-            if(timestop){
-                if(dateeff>=dateajd){
-                   if (repet){}
-                   else
-                    {timestop=false;}
+        var date = new Date(); // Send date
+        var jour= ""+date.getDate();
+
+        if (jour.length==1){var contentdl = 'https://www.duellinksmeta.com/articles/news/' + monthNames[date.getMonth()] + '-' + date.getFullYear()+ '/datamined-leaks-0' + jour;
+        var contentmd = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-0' + jour + '-' +date.getFullYear()+'/master-duel-datamines';
+        var contentcd = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-0' + jour + '-' +date.getFullYear()+'/master-duel-datamines';
+        }
+        else{var contentdl = 'https://www.duellinksmeta.com/articles/news/' + monthNames[date.getMonth()] + '-' + date.getFullYear()+ '/datamined-leaks-' + jour;
+        var contentmd = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-' + jour + '-' +date.getFullYear()+'/master-duel-datamines';
+        var contentcd = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-' + jour + '-' +date.getFullYear()+'/master-duel-datamines';
+        };
+
+        // Duel Links
+        needle.get(contentdl, (error, response) => {
+            var textdl = response.body;
+            if((textdl.includes("Page not found"))||timestopduellinks){        
+                if (date.getHours() ===1){timestopduellinks=false;}
+                else {timestopduellinks=true}
                 }
-                else{repet=false;}
+            else {timestopduellinks=true;   console.log("on rentre");
+ 
+                var avtdl= textdl.indexOf('<meta property=\u0022og:image\u0022 content=\u0022');
+                var imgdl=textdl.substring(avtdl+35);
+                var apdl= imgdl.indexOf('\u0022 data-svelte=\u0022');
+                imgdl=imgdl.substring(0,apdl);
+ 
+                    var embeddl = new EmbedBuilder()
+                        .setColor("#001427")
+                        .setTitle("Duel Links News !")
+                        .setURL(contentdl)
+                        .setDescription("Viens voir les news ici ! \n")
+                        .setImage(imgdl);
+                    client.channels.cache.get(idsalonnewsdl).send({embeds:[embeddl]})};})
+
+        // BanList
+        needle.get(sitekonami, (error, response) => {
+             var dateajd = new Date(date.getFullYear()+'-'+monthNames[date.getMonth()]+'-'+date.getDate());
+             var textban = response.body;
+             var avtban= textban.indexOf("Effectif du ");
+             var effban=textban.substring(avtban+12);
+             var apban= effban.indexOf('</h2>');
+             effban=effban.substring(0,apban);
+             var dateeffban=new Date(effban.substring(6,10)+'-'+effban.substring(3,5)+'-'+effban.substring(0,2));
+        
+            if(timestopban){
+                if(dateeffban>=dateajd){
+                   if (repetban){}
+                   else
+                    {timestopban=false;}
+                }
+                else{repetban=false;}
             }
             else {
-                const embed = new EmbedBuilder()
+                var embedban = new EmbedBuilder()
                         .setColor("#e60026")
                         .setTitle("Nouvelle BanList !")
                         .setURL(sitekonami)
                         .setDescription("Konami a encore frappé ! Venez découvrir quelles sont les cartes bannies et limitées ! \n")
                         .setImage("https://static.wikia.nocookie.net/yugioh-gx/images/6/6f/Prohibition.jpg/revision/latest?cb=20131208095912&path-prefix=fr");
-                    client.channels.cache.get(idsalonnews).send({embeds:[embed]})} 
-                timestop=true;
-                repet=true;
+                    client.channels.cache.get(idsalonnewstcg).send({embeds:[embedban]})
+                    timestopban=true;
+                    repetban=true;
+                }
+        })
+
+        // Master Duel
+        needle.get(contentmd, (error, response) => {
+            var textmd = response.body;
+            if((textmd.includes("Page not found"))||timestopmd){        
+                if (date.getHours() ===1){timestopmd=false;}
             }
-        )
-        }
-        ,1000)
+           else {timestopmd=true;
+               var avtmd= textmd.indexOf('<meta property=\u0022og:image\u0022 content=\u0022');
+               var imgmd=textmd.substring(avtmd+35);
+               var apmd= imgmd.indexOf('\u0022 data-svelte=\u0022');
+               imgmd=imgmd.substring(0,apmd);
+
+               var embedmd = new EmbedBuilder()
+                               .setColor("#5c3a93")
+                               .setTitle("Master Duel News !")
+                               .setURL(contentmd)
+                               .setDescription("De nouveaux ajouts ! \n")
+                               .setImage(imgmd);
+               client.channels.cache.get(idsalonnewsmd).send({embeds:[embedmd]})}
+        })
+
+        // Cross Duel
+
+        needle.get(contentcd, (error, response) => {
+            var textcd = response.body;
+            if((textcd.includes("Page not found"))||timestopcd){        
+                if (date.getHours() ===1){timestopcd=false;}
+            }
+                else {timestopcd=true;
+                    var avtcd= textcd.indexOf('<meta property=\u0022og:image\u0022 content=\u0022');
+                    var imgcd=textcd.substring(avtcd+35);
+                    var apcd= imgcd.indexOf('\u0022 data-svelte=\u0022');
+                    imgcd=imgcd.substring(0,apcd);
+     
+                    const embedcd = new EmbedBuilder()
+                                    .setColor("#ff8000")
+                                    .setTitle("Cross Duel News !")
+                                    .setURL(contentcd)
+                                    .setDescription("Des nouveautés ! \n")
+                                    .setImage(imgcd);
+                    client.channels.cache.get(idsalonnewscd).send({embeds:[embedcd]})}
+        })    
     }
+    ,1000);
+}
 );
 
-// News Duel Links Meta
-
-client.on("ready", message=>{
-    let timestop=false;
-    setInterval( function(){
-        let date = new Date();
-        // Send date
-        let content = 'https://www.duellinksmeta.com/articles/news/' + monthNames[date.getMonth()] + '-' + date.getFullYear()+ '/datamined-leaks-' + date.getDate();
-        needle.get(content, (error, response) => {
-        let text = response.body;
-        if((text.includes("Page not found"))||timestop){        
-            let date = new Date();
-            if (date.getHours() ===1){timestop=false;}
-    }
-            else {timestop=true;
-                let avt= text.indexOf('<meta property=\u0022og:image\u0022 content=\u0022');
-                var img=text.substring(avt+35);
-                let ap= img.indexOf('\u0022 data-svelte=\u0022');
-                img=img.substring(0,ap);
-                const idsalonnews="993787860048150569"; // Mettre ID du salon des news
-
-                    const embed = new EmbedBuilder()
-                        .setColor("#001427")
-                        .setTitle("Duel Links News !")
-                        .setURL(content)
-                        .setDescription("Viens voir les news ici ! \n")
-                        .setImage(img);
-                    client.channels.cache.get(idsalonnews).send({embeds:[embed]})} 
-            })
-        }
-        ,1000
-        );
-        }
-    );
-// Master Duel Links Meta
-
-client.on("ready", message=>{
-    let timestop=false;
-    setInterval( function(){
-        let date = new Date();
-        // Send date
-        let content = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-' + date.getDate() + '-' +date.getFullYear()+'/master-duel-datamines'
-        needle.get(content, (error, response) => {
-        let text = response.body;
-        if((text.includes("Page not found"))||timestop){        
-            let date = new Date();
-            if (date.getHours() ===1){timestop=false;}
-        }
-            else {timestop=true;
-                let avt= text.indexOf('<meta property=\u0022og:image\u0022 content=\u0022');
-                var img=text.substring(avt+35);
-                let ap= img.indexOf('\u0022 data-svelte=\u0022');
-                img=img.substring(0,ap);
-
-                const idsalonnews="993789778833195138"; // Mettre ID du salon des news
-
-
-                var embed = new EmbedBuilder()
-                                .setColor("#5c3a93")
-                                .setTitle("Master Duel News !")
-                                .setURL(content)
-                                .setDescription("De nouveaux ajouts ! \n")
-                                .setImage(img);
-                client.channels.cache.get(idsalonnews).send({embeds:[embed]})} 
-                    })
-            }        ,1000
-            );
-        }
-        );
-
-// Cross Duel Meta
-
-client.on("ready", message=>{
-    let timestop=false;
-    setInterval( function(){
-        let date = new Date();
-        // Send date
-        let content ='https://www.crossduelmeta.com/articles/news/'+ monthNames[date.getMonth()] +'-' + date.getFullYear()+'/datamined-leaks-'+date.getDate();
-        needle.get(content, (error, response) => {
-        let text = response.body;
-        if((text.includes("Page not found"))||timestop){        
-            let date = new Date();
-            if (date.getHours() ===1){timestop=false;}
-        }
-            else {timestop=true;
-                let avt= text.indexOf('<meta property=\u0022og:image\u0022 content=\u0022');
-                var img=text.substring(avt+35);
-                let ap= img.indexOf('\u0022 data-svelte=\u0022');
-                img=img.substring(0,ap);
-
-                const idsalonnews="993789979371249745"; // Mettre ID du salon des news
-
-
-                var embed = new EmbedBuilder()
-                                .setColor("#ff8000")
-                                .setTitle("Cross Duel News !")
-                                .setURL(content)
-                                .setDescription("Des nouveautés ! \n")
-                                .setImage(img);
-                client.channels.cache.get(idsalonnews).send({embeds:[embed]})} 
-                    })
-            },1000
-            );
-        }
-        ); 
 
 
 
