36e5288d2185af56d8b32d48a5d89ee3d18e6bc3
Treaf <117280032+Treaf@users.noreply.github.com>
2022-11-09 18:01:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,12 +37,6 @@ const monthNames = ["january", "february", "march", "april", "may", "june",
     var repetban=true;
 
 
-// News YUGIOH
- 
-
-client.on("ready", message=>{
-    console.log("prêt");
-
     // ID des salons des news
     var idsalonnewstcg="964907351637368832"; 
     var idsalonnewsdl="993787860048150569"; 
@@ -51,34 +45,44 @@ client.on("ready", message=>{
 
     var sitekonami="https://www.yugioh-card.com/fr/limited/";
 
+var contentdl, contentmd, contentcd, textdl, avtdl, imgdl, apdl, imgdl, embeddl, dateajd, textban, avtban, effban, apban, dateeffban, embedban, textmd;
+var avtmd, imgmd, apmd, embedmd, textcd, avtcd, imgcd, apcd, embedcd;
+
+
+// News YUGIOH
+ 
+
+client.on("ready", message=>{
+    console.log("prêt");
+    
     setInterval( function(){
         var date = new Date(); // Send date
         var jour= ""+date.getDate();
 
-        if (jour.length==1){var contentdl = 'https://www.duellinksmeta.com/articles/news/' + monthNames[date.getMonth()] + '-' + date.getFullYear()+ '/datamined-leaks-0' + jour;
-        var contentmd = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-0' + jour + '-' +date.getFullYear()+'/master-duel-datamines';
-        var contentcd = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-0' + jour + '-' +date.getFullYear()+'/master-duel-datamines';
+        if (jour.length==1){contentdl = 'https://www.duellinksmeta.com/articles/news/' + monthNames[date.getMonth()] + '-' + date.getFullYear()+ '/datamined-leaks-0' + jour;
+        contentmd = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-0' + jour + '-' +date.getFullYear()+'/master-duel-datamines';
+        contentcd = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-0' + jour + '-' +date.getFullYear()+'/master-duel-datamines';
         }
-        else{var contentdl = 'https://www.duellinksmeta.com/articles/news/' + monthNames[date.getMonth()] + '-' + date.getFullYear()+ '/datamined-leaks-' + jour;
-        var contentmd = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-' + jour + '-' +date.getFullYear()+'/master-duel-datamines';
-        var contentcd = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-' + jour + '-' +date.getFullYear()+'/master-duel-datamines';
+        else{contentdl = 'https://www.duellinksmeta.com/articles/news/' + monthNames[date.getMonth()] + '-' + date.getFullYear()+ '/datamined-leaks-' + jour;
+        contentmd = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-' + jour + '-' +date.getFullYear()+'/master-duel-datamines';
+        contentcd = 'https://www.masterduelmeta.com/articles/news/'+monthNames[date.getMonth()] + '-' + jour + '-' +date.getFullYear()+'/master-duel-datamines';
         };
 
         // Duel Links
         needle.get(contentdl, (error, response) => {
-            var textdl = response.body;
+            textdl = response.body;
             if((textdl.includes("Page not found"))||timestopduellinks){        
                 if ((date.getHours() ===1)&&(date.getMinutes()<1)){timestopduellinks=false;}
                 else {timestopduellinks=true}
                 }
             else {timestopduellinks=true;  
  
-                var avtdl= textdl.indexOf('<meta property=\u0022og:image\u0022 content=\u0022');
-                var imgdl=textdl.substring(avtdl+35);
-                var apdl= imgdl.indexOf('\u0022 data-svelte=\u0022');
+                avtdl= textdl.indexOf('<meta property=\u0022og:image\u0022 content=\u0022');
+                imgdl=textdl.substring(avtdl+35);
+                apdl= imgdl.indexOf('\u0022 data-svelte=\u0022');
                 imgdl=imgdl.substring(0,apdl);
  
-                    var embeddl = new EmbedBuilder()
+                    embeddl = new EmbedBuilder()
                         .setColor("#001427")
                         .setTitle("Duel Links News !")
                         .setURL(contentdl)
@@ -88,13 +92,13 @@ client.on("ready", message=>{
 
         // BanList
         needle.get(sitekonami, (error, response) => {
-             var dateajd = new Date(date.getFullYear()+'-'+monthNames[date.getMonth()]+'-'+date.getDate());
-             var textban = response.body;
-             var avtban= textban.indexOf("Effectif du ");
-             var effban=textban.substring(avtban+12);
-             var apban= effban.indexOf('</h2>');
+             dateajd = new Date(date.getFullYear()+'-'+monthNames[date.getMonth()]+'-'+date.getDate());
+             textban = response.body;
+             avtban= textban.indexOf("Effectif du ");
+             effban=textban.substring(avtban+12);
+             apban= effban.indexOf('</h2>');
              effban=effban.substring(0,apban);
-             var dateeffban=new Date(effban.substring(6,10)+'-'+effban.substring(3,5)+'-'+effban.substring(0,2));
+             dateeffban=new Date(effban.substring(6,10)+'-'+effban.substring(3,5)+'-'+effban.substring(0,2));
         
             if(timestopban){
                 if(dateeffban>=dateajd){
@@ -105,7 +109,7 @@ client.on("ready", message=>{
                 else{repetban=false;}
             }
             else {
-                var embedban = new EmbedBuilder()
+                embedban = new EmbedBuilder()
                         .setColor("#e60026")
                         .setTitle("Nouvelle BanList !")
                         .setURL(sitekonami)
@@ -119,17 +123,17 @@ client.on("ready", message=>{
 
         // Master Duel
         needle.get(contentmd, (error, response) => {
-            var textmd = response.body;
+            textmd = response.body;
             if((textmd.includes("Page not found"))||timestopmd){        
                 if ((date.getHours() ===1)&&(date.getMinutes()<1)){timestopmd=false;}
             }
            else {timestopmd=true;
-               var avtmd= textmd.indexOf('<meta property=\u0022og:image\u0022 content=\u0022');
-               var imgmd=textmd.substring(avtmd+35);
-               var apmd= imgmd.indexOf('\u0022 data-svelte=\u0022');
+               avtmd= textmd.indexOf('<meta property=\u0022og:image\u0022 content=\u0022');
+               imgmd=textmd.substring(avtmd+35);
+               apmd= imgmd.indexOf('\u0022 data-svelte=\u0022');
                imgmd=imgmd.substring(0,apmd);
 
-               var embedmd = new EmbedBuilder()
+               embedmd = new EmbedBuilder()
                                .setColor("#5c3a93")
                                .setTitle("Master Duel News !")
                                .setURL(contentmd)
@@ -138,20 +142,21 @@ client.on("ready", message=>{
                client.channels.cache.get(idsalonnewsmd).send({embeds:[embedmd]})}
         })
 
+        
         // Cross Duel
 
         needle.get(contentcd, (error, response) => {
-            var textcd = response.body;
+            textcd = response.body;
             if((textcd.includes("Page not found"))||timestopcd){        
                 if ((date.getHours() ===1)&&(date.getMinutes()<1)){timestopcd=false;}
             }
                 else {timestopcd=true;
-                    var avtcd= textcd.indexOf('<meta property=\u0022og:image\u0022 content=\u0022');
-                    var imgcd=textcd.substring(avtcd+35);
-                    var apcd= imgcd.indexOf('\u0022 data-svelte=\u0022');
+                    avtcd= textcd.indexOf('<meta property=\u0022og:image\u0022 content=\u0022');
+                    imgcd=textcd.substring(avtcd+35);
+                    apcd= imgcd.indexOf('\u0022 data-svelte=\u0022');
                     imgcd=imgcd.substring(0,apcd);
      
-                    const embedcd = new EmbedBuilder()
+                    embedcd = new EmbedBuilder()
                                     .setColor("#ff8000")
                                     .setTitle("Cross Duel News !")
                                     .setURL(contentcd)
